ca6135aefcac71700240e13450297ce5bdb02412
theurikenneth <theurikenneth@gmail.com>
2021-08-11 15:36:07 +0000

    AirBnB clone - MySQL

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This is a file storage that has FileStorage class, it serializes instances to JSON format and deserializes JSON back to instances
+This is a file storage that has FileStorage class, it serializes instances to
+JSON format and deserializes JSON back to instances
 """
 import json
 import os.path
@@ -11,7 +12,8 @@ import datetime
 class FileStorage:
 
     """
-    Defination of FileStorage class that serializes instances to JSON and deserializing JSON back to instances.
+    Definition of FileStorage class that serializes instances
+    to JSON and deserializing JSON back to instances.
     """
 
     __file_path = "file.json"
@@ -20,48 +22,55 @@ class FileStorage:
 #    def __init__(self):
 #        super().__init__()
 
-   # @property
+    # @property
     def all(self, cls=None):
         """ Returns the dictionary of models currently in storage """
-        if cls is None:
+        if cls:
+            dic = {}
+            for key, val in self.__objects.items():
+                if type(val) == cls:
+                    dic[key] = self.__objects[key]
+            return dic
+        else:
             return self.__objects
-        return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
-    
-   # @new.setter
+
+    # @new.setter
     def new(self, obj):
         """
-              Puts a new object instance in the '__objects' dictonary with <obj class name >.is as the instance key.
+        Puts a new object instance in the '__objects' dictonary with
+        <obj class name >.is as the instance key.
         Attributes:
               obj (object): object to be set in the _-objects dictionary.
-    
+
         """
         FileStorage.__objects[obj.id] = obj
 
     def save(self):
         """
-            Serializes oobjects in __objects to JSON and into the Json file in __file_path.
+        Serializes oobjects in __objects to JSON and into the
+        Json file in __file_path.
         """
         context_obj = {}
 #        list1 = ["created_at", "updated_at"]
         for key in FileStorage.__objects.keys():
-#            if key not in list1:
+            #        if key not in list1:
             context_obj[key] = FileStorage.__objects[key].to_dict()
 #            else:
-#                context_obj[key] = value.isoformat() 
-            
+#                context_obj[key] = value.isoformat()
+
         with open(FileStorage.__file_path, "w") as f:
             json.dump(context_obj, f)
-                
+
     def reload(self):
         """
             Deserializes the JSON file to __objects if __file_path exists.
             Otherwise does nothing.
         """
         try:
-#        if os.path.isfile(FileStorage.__file_path):
+            #        if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r', encoding='UTF-8') as f:
                 context2 = json.load(f)
-            
+
                 for key in context2.keys():
                     new_value = context2[key]
                     clss = new_value['__class__']
@@ -78,3 +87,7 @@ class FileStorage:
                 del self.__objects[key]
             except KeyError:
                 pass
+
+    def close(self):
+        """ reload method"""
+        self.reload()
